e428a8f539989efbc9689a31fd28049f591ac787
mjemons <martin.emons@gmail.com>
2024-04-25 11:23:25 +0200

    added plotting functionality of spatial metric

---
+++ R/plotMetric.R
@@ -0,0 +1,31 @@
+#' Plot a spatial metric per field of view
+#'
+#' @param spe a spatial experiment object
+#' @param selection the mark(s) you want to compare
+#' @param fun the spatstat function to compute on the point pattern object
+#' @param marks the marks to consider e.g. cell types
+#' @param r_seq the range of r values to compute the function over
+#' @param ncores the number of cores to use for parallel processing, default is 1
+#' @param correction the border correction to apply
+#' @param x the x-axis variable
+#'
+#' @return a ggplot object
+#' @export
+#'
+#' @examples
+#'  spe <- imcdatasets::Damond_2019_Pancreas("spe", full_dataset = FALSE)
+#'  p <- plotMetricPerFov(spe, c('alpha', 'beta'), fun = 'Gcross'
+#'  , marks = 'cell_type', r_seq = seq(0,50, length.out = 50), ncores = 2,
+#'  correction = 'rs', x = 'r')
+#'  print(p)
+#' @import dplyr ggplot2
+plotMetricPerFov <- function(spe, selection, fun, marks = NULL, r_seq = NULL, ncores = 1, correction = NULL, x = NULL){
+  metric_df <- calcMetricPerFov(spe, selection, fun, marks, r_seq, ncores = ncores)
+  p <- ggplot(metric_df, aes(x = .data[[x]], y = .data[[correction]], color = factor(image_id))) +
+    geom_line() +
+    facet_wrap(~patient_id) +
+    theme_minimal() +
+    theme(legend.position = "none") +
+    labs(title = paste0(fun, ' metric for ', paste(selection, collapse = ' and ')))
+  return(p)
+}
