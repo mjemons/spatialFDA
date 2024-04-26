1ef65d1581d88a778ad8b8a7f4a46017b4937646
mjemons <martin.emons@gmail.com>
2024-04-26 12:47:10 +0200

    added functional GAM function

---
+++ R/funcGam.R
@@ -0,0 +1,76 @@
+#' General additive model with functional response
+#'
+#' @param data a dataframe with the following columns: Y = functional response;
+#' sample_id = sample ID; image_id = image ID;
+#' @param x the x-axis values of the functional response
+#' @param designmat a design matrix as defined by model.matrix()
+#' @param weights weights as the number of points per image. These weights are
+#' normalised by the mean of the weights in the fitting process
+#' @param formula the formula for the model. The colnames of the designmatrix
+#' have to correspond to the variables in the formula.
+#'
+#' @return a fitted pffr object which inherits from gam
+#' @export
+#'
+#' @examples
+#'  #load the pancreas dataset
+#'  library('tidyr')
+#'  library('stringr')
+#'  library('dplyr')
+#'  spe <- imcdatasets::Damond_2019_Pancreas("spe", full_dataset = FALSE)
+#'  #calculate the Gcross metric for alpha and beta cells
+#'  metric_res <- calcMetricPerFov(spe, c('alpha', 'beta'), fun = 'Gcross',
+#'  marks = 'cell_type', r_seq = seq(0,50, length.out = 50), ncores = 2)
+#'  metric_res$ID <- paste0(metric_res$condition,'x' ,metric_res$patient_id,
+#'  'x',metric_res$image_id)
+#'  #extract the functional response matrix
+#'  mat <- metric_res %>% select(ID, r, rs) %>%
+#'  spread(ID, rs) %>% select(!r)
+#'  #extract the number of points as weights - are ordered differently,
+#'  #thus order according to image ID
+#'  weights <- metric_res %>% group_by(ID) %>%
+#'  select(ID, npoints) %>% unique() %>% arrange(ID)
+#'  #create a dataframe as required by pffr
+#'  dat <-  data.frame(ID = colnames(mat))
+#'  dat$Y = t(mat)
+#'  #create meta info of the IDs
+#'  split_data <- str_split(dat$ID, "x")
+#'  dat$condition <- factor(sapply(split_data, `[`, 1))
+#'  dat$patient_id <- factor(sapply(split_data, `[`, 2))
+#'  dat$image_id <- factor(sapply(split_data, `[`, 3))
+#'  #create a designmatrix
+#'  condition <- dat$condition
+#'  condition <- relevel(condition, 'Non-diabetic')
+#'  designmat <- model.matrix( ~ condition)
+#'  #colnames don't work with the '-' sign
+#'  colnames(designmat) <- c('(Intercept)', 'conditionLong_duration', 'conditionOnset')
+#'  #fit the model
+#'  mdl <- functionalGam(dat = dat, x = metric_res$r |> unique(),
+#'  designmat = designmat, weights = weights$npoints,
+#'  formula = formula(Y ~ conditionLong_duration +
+#'  conditionOnset + s(patient_id,  bs='re')))
+#'  summary(mdl)
+#'  plot(mdl)
+
+#' @import dplyr
+
+functionalGam <- function(data, x, designmat, weights, formula){
+  #get the colnames
+  colnam <- colnames(designmat)
+  for(i in 1:length(colnam)){
+    data[[colnam[i]]] <- designmat[,i]
+  }
+  #TODO how to make weighting optional?
+  #normalise the weights
+  weights <- weights/mean(weights)
+  #TODO write a test that the colnames of the designmat correspond to formula
+  startTime <- Sys.time()
+  mdl <- refund::pffr(formula,
+              yind=x,
+              data=data,
+              weights = weights,
+              method = "REML")
+  endTime <- Sys.time()
+  print(endTime-startTime)
+  return(mdl)
+}
