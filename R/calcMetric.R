ba008fc3de1ac01e7355e1156435aa0be5be8886
mjemons <martin.emons@gmail.com>
2024-05-07 11:47:35 +0200

    took out hard-coded variables

--- R/calcMetric.R
+++ R/calcMetric.R
@@ -6,29 +6,33 @@
 #' @param fun the spatstat function to compute on the point pattern object
 #' @param marks the marks to consider e.g. cell types
 #' @param r_seq the range of r values to compute the function over
+#' @param by the spe colData variable(s) to add to the meta data
 #'
-#' @return a spatstat metric object with the fov number, the number of points and the centroid of the image
+#' @return a spatstat metric object with the fov number, the number of
+#' points and the centroid of the image
 #' @export
 #'
 #' @examples
 #'  spe <- imcdatasets::Damond_2019_Pancreas("spe", full_dataset = FALSE)
 #'  spe_sub <- subset(spe, ,image_number == '138')
 #'  metric_res <- extractMetric(spe_sub, c('alpha', 'beta'), 138, fun = 'Gcross'
-#'  , marks = 'cell_type', r_seq = seq(0,1000, length.out = 100))
+#'  , marks = 'cell_type', r_seq = seq(0,1000, length.out = 100),
+#'  by = c('patient_stage', 'patient_id'))
 #' @import spatstat.explore
-extractMetric <- function(spe, selection, fov, fun, marks = NULL, r_seq = NULL){
+extractMetric <- function(spe, selection, fov, fun, marks = NULL, r_seq = NULL, by = NULL){
   pp <- .speToppp(spe, marks = marks)
-  patient_id <- spe$patient_id %>% unique()
-  condition <- spe$patient_stage %>% unique()
+  meta_data <- colData(spe)[,by] %>% unique()
   pp_sub <- subset(pp, marks %in% selection, drop = TRUE)
 
-  #small quality control to only consider pp that have more than 100 points per fov and more than one unique mark
-  if(spatstat.geom::npoints(pp_sub)>100 && (length(unique(spatstat.geom::marks(pp_sub)))>1 || length(selection) == 1)){ #& npoints(pp1) > 20 & npoints(pp2) > 20){
-    #TODO: Here I just fix the r values in the range between 0 and 500 to have the same values to compare against in the library fda - that is not ideal --> need to reconsider!
+  #small quality control to only consider pp that have more than 100 points per
+  #fov and more than one unique mark
+  if(spatstat.geom::npoints(pp_sub)>100 &&
+    (length(unique(spatstat.geom::marks(pp_sub)))>1 || length(selection) == 1)){
+    #TODO: Here I just fix the r values in the range between 0 and 500 to have
+    #the same values to compare against in the library fda - that is not ideal
     metric_res <- do.call(fun, args = list(X=pp_sub, r = r_seq))
     metric_res$image_id = fov
-    metric_res$patient_id = patient_id
-    metric_res$condition = condition
+    metric_res = cbind(metric_res, meta_data)
     metric_res$npoints = spatstat.geom::npoints(pp_sub)
     centroid <- spatstat.geom::centroid.owin(pp_sub$window)
     metric_res$centroidx <- centroid$x
@@ -41,10 +45,12 @@ extractMetric <- function(spe, selection, fov, fun, marks = NULL, r_seq = NULL){
 #'
 #' @param spe a spatial experiment object
 #' @param selection the mark(s) you want to compare
+#' @param subsetby the spe colData variable to subset the data by
 #' @param fun the spatstat function to compute on the point pattern object
 #' @param marks the marks to consider e.g. cell types
 #' @param r_seq the range of r values to compute the function over
-#' @param ncores the number of cores to use for parallel processing, default is 1
+#' @param by the spe colData variable(s) to add to the meta data
+#' @param ncores the number of cores to use for parallel processing, default = 1
 #'
 #' @return a dataframe of the spatstat metric objects with the radius r, the
 #' theoretical value of a Poisson process, the different border corrections
@@ -53,14 +59,20 @@ extractMetric <- function(spe, selection, fov, fun, marks = NULL, r_seq = NULL){
 #'
 #' @examples
 #'  spe <- imcdatasets::Damond_2019_Pancreas("spe", full_dataset = FALSE)
-#'  metric_res <- calcMetricPerFov(spe, c('alpha', 'beta'), fun = 'Gcross'
-#'  , marks = 'cell_type', r_seq = seq(0,50, length.out = 50), ncores = 2)
+#'  metric_res <- calcMetricPerFov(spe, c('alpha', 'beta'),
+#'  subsetby = 'image_number',fun = 'Gcross', marks = 'cell_type',
+#'  r_seq = seq(0,50, length.out = 50), by = c('patient_stage', 'patient_id'),
+#'  ncores = 2)
 #' @import dplyr parallel
-calcMetricPerFov <- function(spe, selection, fun, marks = NULL, r_seq = NULL, ncores = 1){
-  fovs <- spe$image_number %>% as.list() %>% unique()
+calcMetricPerFov <- function(spe, selection, subsetby = NULL, fun, marks = NULL,
+                             r_seq = NULL, by = NULL, ncores = 1){
+  fovs <- spe[[subsetby]] %>% unique()
   metric_df <- parallel::mclapply(fovs, function(x){
-    spe_sub <- subset(spe, ,image_number == x)
-    metric_res <- extractMetric(spe = spe_sub, selection = selection, fov = x, fun = fun, marks = marks, r_seq = r_seq) %>% as.data.frame()
+    #this is still hardcoded - I don't know how to subset with string
+    spe_sub <- subset(spe, ,spe[[subsetby]] == x)
+    print(spe_sub)
+    metric_res <- extractMetric(spe = spe_sub, selection = selection, fov = x,
+                                fun = fun, marks = marks, r_seq = r_seq, by = by) %>% as.data.frame()
     return(metric_res)
   }, mc.cores = ncores) %>% bind_rows()
   return(metric_df)
--- R/plotMetric.R
+++ R/plotMetric.R
@@ -2,9 +2,11 @@
 #'
 #' @param spe a spatial experiment object
 #' @param selection the mark(s) you want to compare
+#' @param subsetby the spe colData variable to subset the data by
 #' @param fun the spatstat function to compute on the point pattern object
 #' @param marks the marks to consider e.g. cell types
 #' @param r_seq the range of r values to compute the function over
+#' @param by the spe colData variable(s) to add to the meta data
 #' @param ncores the number of cores to use for parallel processing, default is 1
 #' @param correction the border correction to apply
 #' @param x the x-axis variable
@@ -14,14 +16,14 @@
 #'
 #' @examples
 #'  spe <- imcdatasets::Damond_2019_Pancreas("spe", full_dataset = FALSE)
-#'  p <- plotMetricPerFov(spe, c('alpha', 'beta'), fun = 'Gcross'
-#'  , marks = 'cell_type', r_seq = seq(0,50, length.out = 50), ncores = 2,
-#'  correction = 'rs', x = 'r')
+#'  p <- plotMetricPerFov(spe, c('alpha', 'beta'), subsetby = 'image_number',
+#'  fun = 'Gcross' , marks = 'cell_type', r_seq = seq(0,50, length.out = 50),
+#'  by = c('patient_stage', 'patient_id'), ncores = 2, correction = 'rs', x = 'r')
 #'  print(p)
 #' @import dplyr ggplot2
-plotMetricPerFov <- function(spe, selection, fun, marks = NULL, r_seq = NULL, ncores = 1, correction = NULL, x = NULL){
-  metric_df <- calcMetricPerFov(spe, selection, fun, marks, r_seq, ncores = ncores)
-  metric_df$ID <- paste0(metric_df$condition,'|' ,metric_df$patient_id)
+plotMetricPerFov <- function(spe, selection, subsetby = NULL, fun, marks = NULL, r_seq = NULL, by = NULL, ncores = 1, correction = NULL, x = NULL){
+  metric_df <- calcMetricPerFov(spe, selection, subsetby, fun, marks, r_seq, by = by, ncores = ncores)
+  metric_df$ID <- paste0(metric_df[[by[1]]],'|' ,metric_df[[by[2]]])
   p <- ggplot(metric_df, aes(x = .data[[x]], y = .data[[correction]], color = factor(image_id))) +
     geom_line() +
     facet_wrap(~ID) +
