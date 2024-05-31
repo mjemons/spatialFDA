9b27593f9cbdc0bbf0c6db22634649c396cd7b1c
mjemons <martin.emons@gmail.com>
2024-05-31 16:40:42 +0200

    changes to fPCA plotting and adding fPCA to vignette

--- R/plotMdl.R
+++ R/plotMdl.R
@@ -53,7 +53,6 @@ plotMdl <- function(mdl, predictor){
     geom_ribbon(data=df,aes(ymin=value-1.96*se,ymax=value + 1.96*se),alpha=0.3) +
     geom_hline(yintercept = 0, linetype = 'dashed', color = 'red') +
     ggtitle(predictor) +
-    theme_light() +
-    ylim(-0.5, 0.45)
+    theme_light()
   return(p)
 }
