adef194797c408c3cb506a517844ab8d98bf0bb3
mjemons <martin.emons@gmail.com>
2024-06-12 16:25:37 +0200

    BiocStyle

--- R/prepData.R
+++ R/prepData.R
@@ -10,28 +10,36 @@
 #' @export
 #'
 #' @examples
-#'  spe <- imcdatasets::Damond_2019_Pancreas("spe", full_dataset = FALSE)
-#'  metric_res <- calcMetricPerFov(spe, c('alpha', 'beta'),
-#'  subsetby = 'image_number',fun = 'Gcross', marks = 'cell_type',
-#'  r_seq = seq(0,50, length.out = 50), by = c('patient_stage', 'patient_id'),
-#'  ncores = 2)
+#' spe <- imcdatasets::Damond_2019_Pancreas("spe", full_dataset = FALSE)
+#' metric_res <- calcMetricPerFov(spe, c("alpha", "beta"),
+#'     subsetby = "image_number", fun = "Gcross", marks = "cell_type",
+#'     r_seq = seq(0, 50, length.out = 50), by = c("patient_stage", "patient_id"),
+#'     ncores = 2
+#' )
 #'
-#' #create a unique ID for each row
-#' metric_res$ID <- paste0(metric_res$patient_stage,'x' ,metric_res$patient_id,
-#'                        'x', metric_res$image_id)
-#' dat <- prepData(metric_res, 'r', 'rs')
+#' # create a unique ID for each row
+#' metric_res$ID <- paste0(
+#'     metric_res$patient_stage, "x", metric_res$patient_id,
+#'     "x", metric_res$image_id
+#' )
+#' dat <- prepData(metric_res, "r", "rs")
 #' @import tidyr
-prepData <- function(metric_res, x, y){
-  #extract the functional response matrix
-  mat <- metric_res %>% select(ID, x, y) %>%
-    spread(ID, y) %>% select(!x)
-  #extract the number of points as weights - are ordered differently,
-  #thus order according to image ID
-  weights <- metric_res %>% group_by(ID) %>%
-    select(ID, npoints) %>% unique() %>% arrange(ID)
-  #create a dataframe as required by pffr
-  dat <-  data.frame(ID = colnames(mat))
-  dat$Y <- t(mat)
-  dat$weights <- weights
-  return(dat)
+prepData <- function(metric_res, x, y) {
+    # extract the functional response matrix
+    mat <- metric_res %>%
+        select(ID, x, y) %>%
+        spread(ID, y) %>%
+        select(!x)
+    # extract the number of points as weights - are ordered differently,
+    # thus order according to image ID
+    weights <- metric_res %>%
+        group_by(ID) %>%
+        select(ID, npoints) %>%
+        unique() %>%
+        arrange(ID)
+    # create a dataframe as required by pffr
+    dat <- data.frame(ID = colnames(mat))
+    dat$Y <- t(mat)
+    dat$weights <- weights
+    return(dat)
 }
